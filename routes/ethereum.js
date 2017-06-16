a76c96dab77e7a33f178ab52facc51604eaed700
Danil Vlasov <dvlasov@s-pro.io>
2017-06-16 14:44:14 +0300

    used axios/asyncawait to get actual balance

--- routes/ethereum.js
+++ routes/ethereum.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const router = express.Router();
 
-const getBalance = (req, res, next) => {
-  // tempo. use api call to tonvert
-  const balance = 19254563000000000 / Math.pow(10, 18);
-  res.send({
-    balance
-  })
-}
+const await = require('asyncawait/await');
+const async = require('asyncawait/async');
+const axios = require('axios');
+
+const BASE_URL = `https://api.blockcypher.com/v1/eth/main/addrs/`;
+
+const getBalance = async((req, res, next) => {
+  try {
+    const url = `${BASE_URL}${req.params.id}/balance`;
+    const wallet_balance = await (axios.get(url));
+    const {
+      balance
+    } = wallet_balance.data;
+
+    // 1 Eth = 10^18Wei
+    res.send({
+      balance: balance / Math.pow(10, 18)
+    })
+
+  } catch (e) {
+    const {
+      status,
+      statusText
+    } = e.response ? e.response : {
+      status: 500,
+      statusText: 'Something is wrong'
+    };
+    res.status(status).send(statusText);
+  }
+
+})
 
 /* GET balance. */
 router.get('/balance', getBalance);
