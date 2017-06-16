0867730f1069dbd58d5f81384af77f52eb8dd8ae
Danil Vlasov <dvlasov@s-pro.io>
2017-06-16 15:15:03 +0300

    changed classic functions to arrow functions

--- app.js
+++ app.js
@@ -29,14 +29,14 @@ app.use('/', index);
 app.use('/ethereum', ethereum);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use((req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
